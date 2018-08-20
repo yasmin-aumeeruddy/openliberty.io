f1eddffd11f094c400f9c8d2b63a2b95502bac83
Kin Ueng <kueng@us.ibm.com>
2018-08-20 09:55:47 -0500

    Use Google Closure and csso for minification

--- scripts/build_minification.sh
+++ scripts/build_minification.sh
@@ -1,17 +1,24 @@
-# pushd tools/minification
+mkdir tools
+mkdir tools/minification
 
-# # JS Minification
-# # Google Closure Compiler
-# curl -O https://dl.google.com/closure-compiler/compiler-latest.zip
-# unzip -q compiler-latest.zip -y
-# cd compiler-latest
-# mv closure-compiler-*.jar closure-compiler-latest.jar
-
-# # HTML Minification
-# popd
-
+pushd tools/minification
+# JS Minification
+# Google Closure Compiler
+curl -O https://dl.google.com/closure-compiler/compiler-latest.zip
+unzip -q compiler-latest.zip
+mv closure-compiler-*.jar closure-compiler-latest.jar
+popd
 for js in $(find ./target/jekyll-webapp/assets -name *.js)
 do
         java -jar ./tools/minification/closure-compiler-latest.jar --js $js --js_output_file $js.min
         mv $js.min $js
+done
+
+# CSS minification
+# CSSO Gem
+gem install csso-rails
+for css in $(find ./target/jekyll-webapp/assets -name *.css)
+do
+        ruby_csso $css > $css.min
+        mv $css.min $css
 done
